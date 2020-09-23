16fa91c0dc1c0e4aaa11507507b9312bce2e1fda
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-09-23 16:18:08 +0300

    same problem

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
 import './css/style.css';
+// eslint-disable-next-line import/extensions
 import './js/app';
-import './img.png';
+import './img/arctic';
+
+import './img/characters';
+import './img/desert';
+import './img/mountain';
+import './img/prairie';
 
 // entry point for webpack
 // don't write your code here
